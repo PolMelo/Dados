38112d5110a242e967cbe078e39662b00d586a3d
2812pol <pol.melo14@gmail.com>
2025-10-23 10:45:52 +0200

    d12 y d20 al menu principal

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dadosingle = document.getElementById("dadosingle");
     const dadodouble = document.getElementById("dadodouble");
+    const d12 = document.getElementById("d12");
+    const d20 = document.getElementById("d20");
     if (dadosingle) {
         dadosingle.addEventListener('click', () => {
             window.location.href = "single.html";
@@ -10,6 +12,16 @@ document.addEventListener('DOMContentLoaded', () => {
     if (dadodouble) {
         dadodouble.addEventListener('click', () => {
             window.location.href = "double.html";
+        });
+    }
+    if (d12) {
+        d12.addEventListener('click', () => {
+            window.location.href = "d12.html";
+        });
+    }
+    if (d20) {
+        d20.addEventListener('click', () => {
+            window.location.href = "d20.html";
         });
     }
 });
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dadosingle = document.getElementById("dadosingle");
     const dadodouble = document.getElementById("dadodouble");
+    const d12 = document.getElementById("d12");
+    const d20 = document.getElementById("d20");
 
     if (dadosingle) {
         dadosingle.addEventListener('click', () => {
@@ -13,6 +15,24 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.href = "double.html";
         });
     }
+
+if (d12) {
+        d12.addEventListener('click', () => {
+            window.location.href = "d12.html";
+        });
+    }
+
+    
+if (d20) {
+        d20.addEventListener('click', () => {
+            window.location.href = "d20.html";
+        });
+    }
+
+
+
+
+
 });
 
 
