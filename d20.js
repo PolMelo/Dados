7b90dd8c189ca955fb91ec07ed1b6c937d5693a2
2812pol <pol.melo14@gmail.com>
2025-10-23 10:31:48 +0200

    d12 and d20 html and ts/js

---
+++ d20.js
@@ -0,0 +1,16 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    const tirarun = document.getElementById("tirarun");
+    const fotodado = document.getElementById("dadosoltirar");
+    //Registrar clases per treure anuimacio
+    const dadosoltirar = document.getElementById('dadosoltirar');
+    if (tirarun) {
+        tirarun.onclick = function () {
+            const numero1 = Math.floor(Math.random() * 20) + 1;
+            fotodado.src = `IMG/d${numero1}.png`;
+            //Eliminar la clase para que no gire mas
+            dadosoltirar.classList.remove('dadoModo');
+        };
+    }
+});
+//# sourceMappingURL=d20.js.map
---
+++ src/d12.ts
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const tirarun = document.getElementById("tirarun");
+    const fotodado = document.getElementById("dadosoltirar") as HTMLImageElement;
+
+
+    //Registrar clases per treure anuimacio
+    const dadosoltirar = document.getElementById('dadosoltirar') as HTMLImageElement;
+
+    
+
+    if (tirarun) {
+        tirarun.onclick = function (): void {
+            const numero1 = Math.floor(Math.random() * 12) + 1;
+            fotodado.src = `IMG/d${numero1}.png`;
+
+
+            //Eliminar la clase para que no gire mas
+            dadosoltirar.classList.remove('dadoModo');
+
+           
+        };
+    }
+});
